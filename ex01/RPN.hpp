087c29a8305a94cb7ba139fbdeeb8447a50a44f9
lucia <luciammielgo@gmail.com>
2025-04-06 19:14:50 +0200

    ex01

--- ex01/RPN.hpp
+++ ex01/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: lucia <lucia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 16:51:23 by lucia             #+#    #+#             */
-/*   Updated: 2025/04/05 19:28:17 by lucia            ###   ########.fr       */
+/*   Updated: 2025/04/06 19:08:16 by lucia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,19 @@
 # include <stack>
 # include <cctype>
 
+enum Type {
+	NBR,
+	OP,
+	ERROR
+};
+
 class RPN {
 	private:
 		std::stack<char> _stack;
-		std::stack<char> _calc_stack;
+		std::stack<int> _calc_stack;
 
-		bool ismathop(char c);
+		Type charCheck(const char c);
+		int calculator(const int a, const int b, const char op);
 		
 	public:
 		~RPN(void);
@@ -35,12 +42,19 @@ class RPN {
 		RPN(const RPN &src);
 		RPN &operator=(const RPN &src);
 
+		std::stack<int> getCalcStack(void);
+		
 		//member exceptions
 		class InvalidCharException : public std::exception {
 			public:
 				const char *what() const throw();
 		} ;
-		class InvalidArguments : public std::exception {
+		class InvalidArgumentsException : public std::exception {
+			public:
+				const char *what() const throw();
+		} ;
+
+		class BadStackException : public std::exception {
 			public:
 				const char *what() const throw();
 		} ;
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lucia <lucia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 16:46:37 by lucia             #+#    #+#             */
-/*   Updated: 2025/04/05 18:59:45 by lucia            ###   ########.fr       */
+/*   Updated: 2025/04/06 19:13:27 by lucia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,15 @@
 int main(int argc, char *argv[])
 {
 	if (argc != 2)
-		throw RPN::InvalidArguments();
+		throw RPN::InvalidArgumentsException();
 	
-	RPN rpn(argv[1]);
+	try {
+		RPN rpn(argv[1]);
+		std::cout << "Result : " << rpn.getCalcStack().top() << std::endl;
+	} catch (const std::exception &e){
+		std::cerr << e.what() << std::endl;;
+		std::cerr << 0 << std::endl;
+	}
 	
 	return (0);
 }
